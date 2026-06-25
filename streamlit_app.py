a36fdbffb8c9979ba850ca30820530404f4e9a68
sotilekibru <sotilek@thoughtlogic.com>
2026-06-25 15:24:35 -0500

    Refactor session handling and order submission logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,8 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
@@ -33,11 +34,7 @@ if ingredients_list:
 
     st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
-
-if  time_to_insert:
-    ingredients_string
-    session.sql(my_insert_stmt).collect()
-    
-    st.success('Your Smoothie is ordered!', icon="✅")
-
+    if  time_to_insert:
+     session.sql(my_insert_stmt).collect()
+     st.success('Your Smoothie is ordered!', icon="✅")
     
