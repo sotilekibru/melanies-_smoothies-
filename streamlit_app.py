ed56762693ceebb1df329eedca375478c1871035
sotilekibru <sotilek@thoughtlogic.com>
2026-06-26 09:05:49 -0500

    Update ingredient selection and search logic
    Replace search_pd_df with ingredients_list for user selection.

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop()
 
-search_pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen,'SEARCH_ON'].iloc[0]
+
 ingredients_list = st.multiselect( 
     'Choose up to 5 ingredients:'
     ,my_dataframe
@@ -36,7 +36,8 @@ if ingredients_list:
 
             search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
             st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-         
+            search_pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen,'SEARCH_ON'].iloc[0]
+        
             st.subheader(fruit_chosen + 'Nutrition Information')
             smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
             sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True) 
