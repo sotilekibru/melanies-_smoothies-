4cfec2c7d554d56fcde2c7a9174cc51d2fd895df
sotilekibru <sotilek@thoughtlogic.com>
2026-06-26 08:32:05 -0500

    Replace text display with dataframe for smoothie data
    Comment out the text display of smoothie data and add a dataframe display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,5 +39,6 @@ if ingredients_list:
 
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=TRUE) 
     
